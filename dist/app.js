01890cdf4b8b60b44df43e2f9ee3b73b1f40d383
Abhisheklodha01 <abhisheklodha512@gmail.com>
2024-12-29 15:35:21 +0530

    successfully implemented reverse proxy server

--- dist/app.js
+++ dist/app.js
@@ -12,28 +12,96 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+const node_http_1 = __importDefault(require("node:http"));
 const node_cluster_1 = __importDefault(require("node:cluster"));
 const config_schema_1 = require("./config-schema");
-const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.send("Server is working fine");
-});
+const server_schema_1 = require("./server-schema");
 function createServer(config) {
     return __awaiter(this, void 0, void 0, function* () {
         const { worker_count } = config;
+        const WORKER_POOL = [];
         if (node_cluster_1.default.isPrimary) {
             for (let i = 0; i < worker_count; i++) {
-                node_cluster_1.default.fork({
+                const w = node_cluster_1.default.fork({
                     config: JSON.stringify(config.config),
                 });
+                WORKER_POOL.push(w);
             }
+            const server = node_http_1.default.createServer((req, res) => {
+                const index = Math.floor(Math.random() * WORKER_POOL.length);
+                const worker = WORKER_POOL.at(index);
+                if (!worker) {
+                    throw new Error("Worker not found");
+                }
+                const payload = {
+                    requestType: "HTTP",
+                    headers: req.headers,
+                    body: null,
+                    url: `${req.url}`,
+                };
+                worker.send(JSON.stringify(payload));
+                worker.on("message", (workerReply) => __awaiter(this, void 0, void 0, function* () {
+                    const reply = yield server_schema_1.workerMessageReplySchema.parseAsync(JSON.parse(workerReply));
+                    if (reply.errorCode) {
+                        res.writeHead(parseInt(reply.errorCode));
+                        res.end(reply.error);
+                    }
+                    else {
+                        res.writeHead(200);
+                        res.end(reply.data);
+                        return;
+                    }
+                }));
+            });
+            server.listen(config.port, () => {
+                console.log(`Reverse Proxy Ninja 🥷 ${" "} is Listing on PORT: ${config.port}`);
+            });
         }
         else {
             const configuration = yield config_schema_1.rootConfigSchema.parseAsync(JSON.parse(process.env.config));
-            app.listen(8000, () => {
-                console.log("Server is running on port: 8000");
-            });
+            process.on("message", (message) => __awaiter(this, void 0, void 0, function* () {
+                const validatedMessage = yield server_schema_1.workerMessageSchema.parseAsync(JSON.parse(message));
+                const requestUrl = validatedMessage.url;
+                const rule = config.config.server.rules.find((e) => {
+                    const regex = new RegExp(`^${e.path}.*$`);
+                    return regex.test(requestUrl);
+                });
+                if (!rule) {
+                    const reply = {
+                        errorCode: "404",
+                        error: "Rule not found",
+                    };
+                    if (process.send) {
+                        return process.send(JSON.stringify(reply));
+                    }
+                }
+                const upstreamId = rule === null || rule === void 0 ? void 0 : rule.upstreams[0];
+                const upstream = config.config.server.upstreams.find((e) => e.id === upstreamId);
+                if (!upstream) {
+                    const reply = {
+                        errorCode: "500",
+                        error: "Upstreams not found",
+                    };
+                    if (process.send) {
+                        return process.send(JSON.stringify(reply));
+                    }
+                }
+                const request = node_http_1.default.request({ host: upstream === null || upstream === void 0 ? void 0 : upstream.url, path: requestUrl, method: "GET" }, (proxyRes) => {
+                    let body = "";
+                    proxyRes.on("data", (chunk) => {
+                        body += chunk;
+                    });
+                    proxyRes.on("end", () => {
+                        const reply = {
+                            data: body,
+                        };
+                        if (process.send) {
+                            return process.send(JSON.stringify(reply));
+                        }
+                    });
+                });
+                request.end();
+            }));
         }
     });
 }
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import express from "express";
-import cluster from "node:cluster";
+import http, { request } from "node:http";
+import cluster, { Worker } from "node:cluster";
 import { ConfigSchemaType, rootConfigSchema } from "./config-schema";
-
-const app = express();
-app.get("/", (req, res) => {
-  res.send("Server is working fine");
-});
+import {
+  workerMessageSchema,
+  WorkerMessageType,
+  workerMessageReplySchema,
+  WorkerMessageReplyType,
+} from "./server-schema";
 
 interface serverConfig {
   port: number;
@@ -15,18 +16,100 @@ interface serverConfig {
 
 async function createServer(config: serverConfig) {
   const { worker_count } = config;
+  const WORKER_POOL: Worker[] = [];
   if (cluster.isPrimary) {
     for (let i = 0; i < worker_count; i++) {
-      cluster.fork({
+      const w = cluster.fork({
         config: JSON.stringify(config.config),
       });
+      WORKER_POOL.push(w);
     }
+    const server = http.createServer((req, res) => {
+      const index = Math.floor(Math.random() * WORKER_POOL.length);
+      const worker = WORKER_POOL.at(index);
+      if (!worker) {
+        throw new Error("Worker not found");
+      }
+      const payload: WorkerMessageType = {
+        requestType: "HTTP",
+        headers: req.headers,
+        body: null,
+        url: `${req.url}`,
+      };
+      worker.send(JSON.stringify(payload));
+      worker.on("message", async (workerReply: string) => {
+        const reply: WorkerMessageReplyType =
+          await workerMessageReplySchema.parseAsync(JSON.parse(workerReply));
+        if (reply.errorCode) {
+          res.writeHead(parseInt(reply.errorCode));
+          res.end(reply.error);
+        } else {
+          res.writeHead(200);
+          res.end(reply.data);
+          return;
+        }
+      });
+    });
+    server.listen(config.port, () => {
+      console.log(
+        `Reverse Proxy Ninja 🥷 ${" "} is Listing on PORT: ${config.port}`
+      );
+    });
   } else {
     const configuration = await rootConfigSchema.parseAsync(
       JSON.parse(process.env.config as string)
     );
-    app.listen(8000, () => {
-      console.log("Server is running on port: 8000");
+    process.on("message", async (message: string) => {
+      const validatedMessage = await workerMessageSchema.parseAsync(
+        JSON.parse(message)
+      );
+      const requestUrl = validatedMessage.url;
+      const rule = config.config.server.rules.find((e) => {
+        const regex = new RegExp(`^${e.path}.*$`);
+        return regex.test(requestUrl);
+      });
+      if (!rule) {
+        const reply: WorkerMessageReplyType = {
+          errorCode: "404",
+          error: "Rule not found",
+        };
+        if (process.send) {
+          return process.send(JSON.stringify(reply));
+        }
+      }
+      const upstreamId = rule?.upstreams[0];
+      const upstream = config.config.server.upstreams.find(
+        (e) => e.id === upstreamId
+      );
+      if (!upstream) {
+        const reply: WorkerMessageReplyType = {
+          errorCode: "500",
+          error: "Upstreams not found",
+        };
+        if (process.send) {
+          return process.send(JSON.stringify(reply));
+        }
+      }
+
+      const request = http.request(
+        { host: upstream?.url, path: requestUrl, method: "GET" },
+        (proxyRes) => {
+          let body = "";
+          proxyRes.on("data", (chunk) => {
+            body += chunk;
+          });
+
+          proxyRes.on("end", () => {
+            const reply: WorkerMessageReplyType = {
+              data: body,
+            };
+            if (process.send) {
+              return process.send(JSON.stringify(reply));
+            }
+          });
+        }
+      );
+      request.end();
     });
   }
 }
